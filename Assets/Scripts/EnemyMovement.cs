45c6cf49bc67e589078fb7a1955881befd77993e
tcm12 <tudor_marius_chis@yahoo.com>
2020-12-07 23:59:55 +0200

    initial commit

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Rigidbody2D myRigidBody2D;
+
+    [SerializeField] float movingSpeed = 1f;
+    Vector2 flip = new Vector2(1f, 1f);
+
+    void Start()
+    {
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+        myRigidBody2D.velocity = new Vector2(movingSpeed, 0f);
+    }
+
+    private void flipOnTouch()
+    {
+        myRigidBody2D.velocity = new Vector2((-1) * myRigidBody2D.velocity.x, 0f);
+        flip = new Vector2(flip.x * (-1), 1f);
+        myRigidBody2D.transform.localScale = flip;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        flipOnTouch();
+    }
+}
---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class PlayerData
+{
+    public int level;
+    public int lives;
+    public int resetLives;
+
+    public PlayerData(Player player)
+    {
+        level = player.currentLevel();
+        lives = player.GetLives();
+        resetLives = player.GetResetLives();
+    }
+}
